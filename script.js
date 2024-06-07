c4b9ef78dd3d34a5b485204ba6ec2657dd18ecba
Abdul Samad <samadcom797@gmail.com>
2024-06-07 15:58:11 +0000

    Final commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const tasksList = document.querySelectorAll("[data-task-card]");
+const tabsList = document.querySelectorAll("[data-tab]");
+
+//function to handle tabs
+function handleTabs(currentTab) {
+  const notSelectedTabs = Array.from(tabsList).filter((tab) => tab !== currentTab);
+
+  console.log(notSelectedTabs)
+  notSelectedTabs.forEach((notSelectedTab) => {
+    notSelectedTab.style.color = "hsl(0, 0%, 100%)";
+  });
+
+  currentTab.style.color = " hsl(236, 100%, 87%)";
+}
+
+//fuction to update the UI with data
+function updateData(data, eventObject) {
+  handleTabs(eventObject.currentTarget);
+
+  data.forEach((task, index) => {
+    const timeFrame = task.timeframes;
+    const timeKey = eventObject.currentTarget.innerText.toLowerCase();
+    const time = Object.keys(timeFrame).find((key) => key === timeKey);
+
+    const currentData = tasksList[index].querySelector("[data-task-time]");
+    const previousData = tasksList[index].querySelector("[data-last-week-data]");
+
+    currentData.textContent = `${timeFrame[time].current}hrs`;
+    previousData.textContent = `Previous - ${timeFrame[time].previous}hrs`;
+  });
+}
+
+// async function to fetch data from json file
+async function fetchData() {
+  const response = await fetch("/JSON/data.json");
+  const data = response.json();
+
+  return data;
+}
+
+//function to handle data
+function handleData() {
+  fetchData().then((tasks) => tasks).then((data) => {
+    tabsList.forEach((tab) => tab.addEventListener("click", (e) => updateData(data, e)));
+  });
+}
+
+handleData();
+
+//by default selecting the daily tab on page load and reload
+const setDefaultTab = () => document.querySelector("[data-daily-tab]").click();
+
+window.addEventListener("load", setDefaultTab);
+window.addEventListener("reload", setDefaultTab);
+
+
+
+
